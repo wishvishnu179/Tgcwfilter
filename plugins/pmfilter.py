d31483178e9868a78edc164f2f4283a0c290a652
wishvishnu179 <137043572+wishvishnu179@users.noreply.github.com>
2024-02-20 22:06:19 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1974,7 +1974,6 @@ async def auto_filter(client, msg, spoll=False):
             return
         if len(message.text) < 100:
             search = message.text
-            m=await message.reply_sticker("CAACAgQAAxkBAAEKSxplArIUActk4ORQuFn3DHFvBqQCOgACBQMAAnJxFyVYcSIunXgGjjAE")
             reply_markup={search}
             search = search.lower()
             find = search.split(" ")
@@ -2004,7 +2003,6 @@ async def auto_filter(client, msg, spoll=False):
     else:
         message = msg.message.reply_to_message  # msg will be callback query
         search, files, offset, total_results = spoll
-        m=await message.reply_sticker("CAACAgQAAxkBAAEKSxplArIUActk4ORQuFn3DHFvBqQCOgACBQMAAnJxFyVYcSIunXgGjjAE")
         reply_markup={search}
         settings = await get_settings(message.chat.id)
         await msg.message.delete()
