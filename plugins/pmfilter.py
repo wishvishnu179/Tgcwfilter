1d8699deb6a4fad8d1df998a16761386373f8ce3
wishvishnu179 <137043572+wishvishnu179@users.noreply.github.com>
2024-02-28 23:04:51 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1869,7 +1869,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     ]]
             reply_markup = InlineKeyboardMarkup(buttons)
             await query.message.edit_text(
-                text=script.STICKER_TXT.format(query.from_user.mention),
+                text=script.COUNTRY_TXT.format(query.from_user.mention),
                 reply_markup=reply_markup,
                 parse_mode=enums.ParseMode.HTML
             )
