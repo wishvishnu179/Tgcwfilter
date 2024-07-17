f128e386105c8c06cf7e40ba7f3daeacef57462d
wishvishnu179 <137043572+wishvishnu179@users.noreply.github.com>
2024-07-17 17:07:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -29,8 +29,8 @@ SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://graph.org/file/005ec4a3d918
 CODE = (environ.get('CODE', 'https://graph.org/file/9325fbcf013e395e7249a.jpg'))
 
 #stream link shortner
-STREAM_SITE = (environ.get('STREAM_SITE', ''))
-STREAM_API = (environ.get('STREAM_API', ''))
+STREAM_SITE = (environ.get('STREAM_SITE', 'adrinolinks.in'))
+STREAM_API = (environ.get('STREAM_API', '03deca1ed6ba5b82704afd62a669699427b88214'))
 STREAMHTO = (environ.get('STREAMHTO', 'https://t.me/Shortnerlinksopen/7'))
 
 # Admins, Channels & Users
